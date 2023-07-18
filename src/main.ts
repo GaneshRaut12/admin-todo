e4df65f42a14924a57df7983e21ede4bb7fba941
Ganesh Raut <ganesh.raut@softobiz.com>
2023-07-18 12:25:32 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 declare const module: any;
 async function bootstrap() {
 const app = await NestFactory.create(AppModule, {cors: true});
+
 app.listen(5000)
 .then(() => {
 console.log("successfully stared on port 5000");
